b0f4a4def4efbf124d186547be7007fc3b6e0704
Hüseyin Hamad <huseyinnhamad@gmail.com>
2022-06-06 17:07:44 +0300

    Analysis's added

--- visualization/analysis.py
+++ visualization/analysis.py
@@ -2,5 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    df = pd.read_csv('../data/trackFeatures.csv')
-    print(df.head())
+    df = pd.read_csv("../data/trackFeatures.csv")
+    df.insert(loc=0, column='Playlist Name', value=df['playlistName'])
+    df.insert(loc=1, column='Track Name', value=df['trackName'])
+    df = df.drop(columns=['Unnamed: 0', 'trackName', 'playlistName'])
+
+    fig = plt.figure(figsize=(100, 50))
+    playlists = df['Playlist Name'].unique()
+    means = df.groupby('Playlist Name').mean()
+
+    plt.bar(playlists, means['danceability'], color='maroon',
+            width=0.4)
+    plt.xlabel("User Playlists")
+    plt.xticks(rotation=90)  # Rotates X-Axis Ticks by 45-degrees
+    plt.ylabel("Danceability")
+    plt.title("Playlists / Danceability")
+    plt.show()
